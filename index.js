bddee19ee68b2c9e489e2f07fb25d6e4a7ca5da6
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2022-10-22 23:48:25 +0400

    made the main functions

---
+++ index.js
@@ -0,0 +1,48 @@
+function check() {
+    let textn = document.getElementById("exampleInputName");
+    let texts = document.getElementById("exampleInputSurname");
+    let email = document.getElementById("exampleInputEmail1");
+    let pwd = document.getElementById("exampleInputPassword1");
+    let pwdrepeat = document.getElementById("exampleInputPassword2");
+
+    document.getElementById('errorMessage').innerHTML = "";
+
+    if (textn.value == '') {
+        document.getElementById('errorMessage')
+        .innerHTML+= "Your name is not filled in<br>";
+        }
+
+    if (texts.value == '') {
+        document.getElementById('errorMessage')
+        .innerHTML+= "Your surname is not filled in<br>";
+            }
+        
+    
+    if (email.value == '') {
+        document.getElementById('errorMessage')
+        .innerHTML+= "Your email is not filled in<br>";
+    }
+        
+
+    if (pwd.value == '') {
+        document.getElementById('errorMessage')
+        .innerHTML+= "Password is not filled in<br>";
+    }
+
+    if (pwd.value.length <= 6) {
+        document.getElementById('errorMessage')
+        .innerHTML += "Password is too short<br>";
+    }
+        
+
+    if (pwdrepeat.value == '') {
+        document.getElementById('errorMessage')
+        .innerHTML+= "Password-repeat is not filled in";
+    }  
+
+    if (pwdrepeat.value != pwd.value) {
+        document.getElementById('errorMessage')
+        .innerHTML+= "Passwords don't match";
+    }  
+
+}
